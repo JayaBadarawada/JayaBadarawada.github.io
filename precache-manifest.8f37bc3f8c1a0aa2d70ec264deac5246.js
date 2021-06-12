a4d0aedfaff404ad47491bf1450244fd21347cfa
JAYA <mavuleti.satya@gmail.com>
2021-06-12 19:41:11 +0200

    First commit

---
+++ precache-manifest.8f37bc3f8c1a0aa2d70ec264deac5246.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a458d1447579d0bc74",
+    "url": "/css/app.90410797.css"
+  },
+  {
+    "revision": "4073d6d36c8a5ee86297",
+    "url": "/css/chunk-vendors.e2eba18d.css"
+  },
+  {
+    "revision": "1eb323bbe28a4bd447ea80f8b4f1f23c",
+    "url": "/img/logo1.1eb323bb.png"
+  },
+  {
+    "revision": "1c41fd16abfbf239160c5b4208e320e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0a458d1447579d0bc74",
+    "url": "/js/app.4f62fc03.js"
+  },
+  {
+    "revision": "4073d6d36c8a5ee86297",
+    "url": "/js/chunk-vendors.fa9d94aa.js"
+  },
+  {
+    "revision": "3c49202925dc58086988dd02fec7b7eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
